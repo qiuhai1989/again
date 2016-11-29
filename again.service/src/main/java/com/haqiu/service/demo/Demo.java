537edf489266f81fb2aa43c2960104bec500ec1a
qiuhai <55704123ggdd@163.com>
2016-11-29 10:25:41 +0800

    .

--- again.service/src/main/java/com/haqiu/service/demo/Demo.java
+++ again.service/src/main/java/com/haqiu/service/demo/Demo.java
@@ -4,4 +4,18 @@ package com.haqiu.service.demo;
  * Created by Administrator on 2016/11/25.
  */
 public class Demo {
+
+    /** 
+     *@Description:
+     *@author: 丘海
+     *@date 2016/11/29 10:25
+     *@method 
+     *@param  a
+     *@return  
+    */
+    private int test(int a){
+
+        return a;
+    }
+
 }
